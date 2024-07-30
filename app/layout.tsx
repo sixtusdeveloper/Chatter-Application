b525dfb57325867d5dbb5a866e62779c1d423af8
sixtusdeveloper <sixtusushrey@gmail.com>
2024-07-30 12:05:01 +0100

    commit on few modified files and Clerk authentication

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,55 @@
+// import type { Metadata } from "next";
+// import { Inter } from "next/font/google";
+// import "./globals.css";
+// import { ThemeProvider } from "./provider";
+// import { ClerkProvider } from "@clerk/nextjs";
+// import { dark } from "@clerk/themes";
+
+// const inter = Inter({ subsets: ["latin"] });
+
+// export const metadata: Metadata = {
+//   title: "Chatter Application",
+//   description: "A dynamic and innovative platform designed to cater to the needs of traditional bookworms and modern content creators alike.",
+// };
+
+// export default function RootLayout({
+//   children,
+// }: Readonly<{
+//   children: React.ReactNode;
+// }>) {
+//   return (
+//     <ClerkProvider
+//       appearance={{
+//         baseTheme: dark,
+//         variables: { 
+//           colorPrimary: "#3371FF", fontSize: '16px' , }
+        
+       
+//       }}
+    
+//     >
+//       <html lang="en">
+//         <body className={inter.className}>
+//           <ThemeProvider
+//               attribute="class"
+//               defaultTheme="dark"
+//               enableSystem
+//               disableTransitionOnChange
+//             >
+//               {children}
+              
+//           </ThemeProvider>
+          
+//         </body>
+//       </html>
+//     </ClerkProvider>
+//   );
+// }
+
+
+
+
+
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
@@ -21,24 +73,23 @@ export default function RootLayout({
     <ClerkProvider
       appearance={{
         baseTheme: dark,
-        variables: { 
-          colorPrimary: "#3371FF", fontSize: '16px' , }
-        
-       
+        variables: {
+          colorPrimary: "#3371FF",
+          fontSize: "16px",
+        },
       }}
-    
     >
       <html lang="en">
         <body className={inter.className}>
-          <ThemeProvider
+          
+            <ThemeProvider
               attribute="class"
               defaultTheme="dark"
               enableSystem
               disableTransitionOnChange
             >
               {children}
-              
-          </ThemeProvider>
+            </ThemeProvider>
           
         </body>
       </html>
--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,18 +1,12 @@
 'use client';
 
-// import Link from 'next/link';
-import { useState, useEffect, useRef } from 'react';
+import { useState, useEffect } from 'react';
 import { navigation } from '@/data';
 import { Dialog, DialogPanel } from '@headlessui/react';
 import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline';
 import LogoImg from '@/public/img/logo-white.png'; 
 import LogoImgDark from '@/public/img/logo-black.png';
-// import SignUpButton from './ui/SignupButton';
-// import LoginButton from './ui/LoginButton';
-// import { Sign } from 'crypto';
 import { SignedIn, SignedOut, SignInButton, UserButton } from '@clerk/nextjs';
-
-
 
 export default function Navbar({
     LogoImg, title, LogoImgDark, 
@@ -43,12 +37,11 @@ export default function Navbar({
     const options = {
       root: null,
       rootMargin: '0px',
-      threshold: 0.3 // Adjusted threshold to ensure better detection
+      threshold: 0.3
     };
 
     const observer = new IntersectionObserver((entries) => {
       entries.forEach(entry => {
-        console.log(`Section ${entry.target.id} is intersecting: ${entry.isIntersecting}`); // Debug log
         if (entry.isIntersecting) {
           setActiveSection(entry.target.id);
         }
@@ -67,7 +60,7 @@ export default function Navbar({
   }, []);
 
   const handleClick = (href: string) => {
-    setActiveSection(href.substring(1)); // Remove the '#' from the href to get the id
+    setActiveSection(href.substring(1));
   };
 
   return (
@@ -106,22 +99,12 @@ export default function Navbar({
           ))}
         </div>
         <div className="hidden lg:flex lg:flex-1 lg:justify-end gap-5">
-          {/* <Link href="/regform" legacyBehavior>
-            <SignUpButton 
-            title="Sign up" 
-            />
-          </Link>
-          <LoginButton 
-          title="Log in"
-          /> */}
           <SignedOut>
             <SignInButton />
           </SignedOut>
-
           <SignedIn>
             <UserButton />
           </SignedIn>
-
         </div>
       </nav>
       <Dialog open={mobileMenuOpen} onClose={setMobileMenuOpen} className="lg:hidden">
@@ -160,30 +143,9 @@ export default function Navbar({
                 ))}
               </div>
               <div className="py-3">
-                {/* <SignedOut>
-                  <a
-                    href="#"
-                    className="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-blue-600 hover:purple-8"
-                  >
-                    <SignInButton />
-                     
-                  </a>
-                </SignedOut>
-                
-                <SignedIn>
-                  <a
-                    href=""
-                    className="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-blue-600 hover:purple-8"
-                  >
-                    <UserButton />
-                    Sign in
-                  </a>
-                </SignedIn> */}
-
                 <SignedOut>
                   <SignInButton />
                 </SignedOut>
-
                 <SignedIn>
                   <UserButton />
                 </SignedIn>
@@ -195,5 +157,214 @@ export default function Navbar({
     </header>
   );
 }
+
+
+
+
+
+
+
+// 'use client';
+
+// // import Link from 'next/link';
+// import { useState, useEffect, useRef } from 'react';
+// import { navigation } from '@/data';
+// import { Dialog, DialogPanel } from '@headlessui/react';
+// import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline';
+// import LogoImg from '@/public/img/logo-white.png'; 
+// import LogoImgDark from '@/public/img/logo-black.png';
+// // import SignUpButton from './ui/SignupButton';
+// // import LoginButton from './ui/LoginButton';
+// // import { Sign } from 'crypto';
+// import { SignedIn, SignedOut, SignInButton, UserButton } from '@clerk/nextjs';
+
+
+
+// export default function Navbar({
+//     LogoImg, title, LogoImgDark, 
+// }: {
+//     LogoImg?: string;
+//     title?: string;
+//     LogoImgDark?: string;
+// }) {
+//   const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
+//   const [scrolling, setScrolling] = useState(false);
+//   const [activeSection, setActiveSection] = useState('');
+
+//   useEffect(() => {
+//     const handleScroll = () => {
+//       if (window.scrollY > 0) {
+//         setScrolling(true);
+//       } else {
+//         setScrolling(false);
+//       }
+//     };
+
+//     window.addEventListener('scroll', handleScroll);
+//     return () => window.removeEventListener('scroll', handleScroll);
+//   }, []);
+
+//   useEffect(() => {
+//     const sections = navigation.map(item => document.querySelector(item.href));
+//     const options = {
+//       root: null,
+//       rootMargin: '0px',
+//       threshold: 0.3 // Adjusted threshold to ensure better detection
+//     };
+
+//     const observer = new IntersectionObserver((entries) => {
+//       entries.forEach(entry => {
+//         console.log(`Section ${entry.target.id} is intersecting: ${entry.isIntersecting}`); // Debug log
+//         if (entry.isIntersecting) {
+//           setActiveSection(entry.target.id);
+//         }
+//       });
+//     }, options);
+
+//     sections.forEach(section => {
+//       if (section) observer.observe(section);
+//     });
+
+//     return () => {
+//       sections.forEach(section => {
+//         if (section) observer.unobserve(section);
+//       });
+//     };
+//   }, []);
+
+//   const handleClick = (href: string) => {
+//     setActiveSection(href.substring(1)); // Remove the '#' from the href to get the id
+//   };
+
+//   return (
+//     <header className={`fixed inset-x-0 top-0 z-50 transition-colors duration-500 ${scrolling ? 'bg-black-200' : 'bg-transparent'}`}>
+//       <nav aria-label="Global" className="flex items-center justify-between p-4 lg:px-8">
+//         <div className="flex lg:flex-1">
+//           <a href="/" className="-m-1.5 p-1.5">
+//             <span className="sr-only">Your Company</span>
+//             <img
+//               alt="Chatter"
+//               src={LogoImg || "/img/logo-black.png"}
+//               className="h-16 w-auto"
+//             />
+//           </a>
+//         </div>
+//         <div className="flex lg:hidden">
+//           <button
+//             type="button"
+//             onClick={() => setMobileMenuOpen(true)}
+//             className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-blue-100"
+//           >
+//             <span className="sr-only">Open main menu</span>
+//             <Bars3Icon aria-hidden="true" className="h-6 w-6" />
+//           </button>
+//         </div>
+//         <div className="hidden lg:flex lg:gap-x-12">
+//           {navigation.map((item) => (
+//             <a
+//               key={item.name}
+//               href={item.href}
+//               className={`text-sm font-semibold leading-6 tracking-wider ${activeSection === item.href.substring(1) ? 'text-blue-300' : 'text-blue-100'} hover:text-blue-200`}
+//               onClick={() => handleClick(item.href)}
+//             >
+//               {item.name}
+//             </a>
+//           ))}
+//         </div>
+//         <div className="hidden lg:flex lg:flex-1 lg:justify-end gap-5">
+//           {/* <Link href="/regform" legacyBehavior>
+//             <SignUpButton 
+//             title="Sign up" 
+//             />
+//           </Link>
+//           <LoginButton 
+//           title="Log in"
+//           /> */}
+//           <SignedOut>
+//             <SignInButton />
+//           </SignedOut>
+
+//           <SignedIn>
+//             <UserButton />
+//           </SignedIn>
+
+//         </div>
+//       </nav>
+//       <Dialog open={mobileMenuOpen} onClose={setMobileMenuOpen} className="lg:hidden">
+//         <div className="fixed inset-0 z-50" />
+//         <DialogPanel className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-blue-100 px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
+//           <div className="flex items-center justify-between">
+//             <a href="#" className="-m-1.5 p-1.5">
+//               <span className="sr-only">Your Company</span>
+//               <img
+//                 alt="Chatter"
+//                 src={LogoImgDark || "/img/logo-white.png"}
+//                 className="h-16 w-auto"
+//               />
+//             </a>
+//             <button
+//               type="button"
+//               onClick={() => setMobileMenuOpen(false)}
+//               className="-m-2.5 rounded-md p-2.5 text-black-100"
+//             >
+//               <span className="sr-only">Close menu</span>
+//               <XMarkIcon aria-hidden="true" className="h-6 w-6" />
+//             </button>
+//           </div>
+//           <div className="mt-6 flow-root">
+//             <div className="-my-6 divide-y divide-gray-500/10">
+//               <div className="space-y-2 py-6">
+//                 {navigation.map((item) => (
+//                   <a
+//                     key={item.name}
+//                     href={item.href}
+//                     className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-black-100 hover:tpurple-8"
+//                     onClick={() => handleClick(item.href)}
+//                   >
+//                     {item.name}
+//                   </a>
+//                 ))}
+//               </div>
+//               <div className="py-3">
+//                 {/* <SignedOut>
+//                   <a
+//                     href="#"
+//                     className="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-blue-600 hover:purple-8"
+//                   >
+//                     <SignInButton />
+                     
+//                   </a>
+//                 </SignedOut>
+                
+//                 <SignedIn>
+//                   <a
+//                     href=""
+//                     className="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-blue-600 hover:purple-8"
+//                   >
+//                     <UserButton />
+//                     Sign in
+//                   </a>
+//                 </SignedIn> */}
+
+//                 <SignedOut>
+//                   <SignInButton />
+//                 </SignedOut>
+
+//                 <SignedIn>
+//                   <UserButton />
+//                 </SignedIn>
+//               </div>
+//             </div>
+//           </div>
+//         </DialogPanel>
+//       </Dialog>
+//     </header>
+//   );
+// }
+
+
+
+
+
 
 
