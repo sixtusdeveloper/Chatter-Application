3e292bc329b5612da27ee56d9c438c197e3b8f5e
sixtusdeveloper <sixtusushrey@gmail.com>
2024-08-02 00:07:23 +0100

    commit on many files

--- components/ui/ImageWithFallback.tsx
+++ components/ui/ImageWithFallback.tsx
@@ -1,26 +1,3 @@
-// 'use client';
-
-// import { useState } from 'react';
-// import Image from 'next/image';
-
-// const ImageWithFallback = ({ src, alt, fallbackSrc, className }) => {
-//   const [imgSrc, setImgSrc] = useState(src);
-
-//   return (
-//     <Image
-//       src={imgSrc}
-//       alt={alt}
-//       className={className}
-//       loading="lazy"
-//       onError={() => setImgSrc(fallbackSrc)}
-//     />
-//   );
-// };
-
-// export default ImageWithFallback;
-
-
-
 // Updated code 
 'use client';
 import React from 'react';
